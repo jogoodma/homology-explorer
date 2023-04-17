93c9d8106b214ee51da9d6bf5b99899727cd8454
Josh Goodman <jogoodma@iu.edu>
2023-04-16 20:31:36 -0400

    Centered gene search form

--- apps/ui/src/pages/index.tsx
+++ apps/ui/src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
     .slice(0, 19);
 
   return (
-    <section id="search" className="mt-20 w-3/5 p-8">
+    <section id="search" className="mt-20 p-8 flex justify-center">
       <GeneSearch onChange={(value) => setGene(value)} />
       <GeneResults results={geneResults} />
     </section>
